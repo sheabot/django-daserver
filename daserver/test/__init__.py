e3f30a4b74f6aefefefc6854d4fc791e99998b6f
Ryan Shea <shearyanp@gmail.com>
2017-11-13 21:11:26 -0700

    daserver: test: Added DASD_LOGGING environment variable

--- daserver/test/__init__.py
+++ daserver/test/__init__.py
@@ -1,15 +1,19 @@
 import logging
+import os
 
 def configure_logging():
     # Get logger and set format
     logger = logging.getLogger("dasd")
     logger.setLevel(logging.DEBUG)
-    formatter = logging.Formatter("%(asctime)s - %(levelname)5s - %(funcName)s: %(message)s",
-                                    datefmt="%Y-%m-%d %H:%M:%S")
+    formatter = logging.Formatter(
+        "%(asctime)s [%(levelname)5s] %(funcName)s: %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S"
+    )
 
     # Setup StreamHandler()
     handler = logging.StreamHandler()
     handler.setFormatter(formatter)
     logger.addHandler(handler)
 
-#configure_logging()
+if bool(os.getenv('DASD_LOGGING', False)):
+    configure_logging()
